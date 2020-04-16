b40b9dd70286542fb54e94c0a17fc0342bb2ec7f
Zane Mohammed <zanemohammed2016@gmail.com>
2020-04-16 01:08:56 -0400

    Updates

---
+++ precache-manifest.877a13844e598fcb57a8a44c5c19efc9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f2999bb932921accf9503edf862daa5",
+    "url": "/PiFarm/index.html"
+  },
+  {
+    "revision": "ab9caced75a64aa0ae20",
+    "url": "/PiFarm/static/js/2.143810b9.chunk.js"
+  },
+  {
+    "revision": "758f315ca4a196d985645f6e6df3f8f3",
+    "url": "/PiFarm/static/js/2.143810b9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b8083ff13604edb4a5c",
+    "url": "/PiFarm/static/js/main.d8f75d7c.chunk.js"
+  },
+  {
+    "revision": "5bfb4009415b4eb93029",
+    "url": "/PiFarm/static/js/runtime-main.382962dd.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b2363a03375a00f95afff610a1edd328.js"
+  "/PiFarm/precache-manifest.877a13844e598fcb57a8a44c5c19efc9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/PiFarm/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
